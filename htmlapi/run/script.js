3bf03de0a0bd2f3864cb98d8f98ef7441e86a9d7
jaxodotdev-downloadholder <98191771+jaxodotdev-downloadholder@users.noreply.github.com>
2022-01-29 15:52:17 -0500

    Add files via upload

---
+++ htmlapi/run/script.js
@@ -0,0 +1,26 @@
+const query = window.location.search;
+console.log("Searched URL. Got output:\n"+query);
+
+const params = new URLSearchParams(query);
+console.log("Found parameters. Got output:\n"+params);
+
+const product = params.get('text');
+console.log("Found value. Got output:\n"+product);
+
+const wname = params.get('name');
+const wavatar = params.get('pfp');
+const wurl = params.get('url');
+
+function sendMessage(msg) {
+  const request = new XMLHttpRequest();
+  request.open("POST", wurl);
+  request.setRequestHeader('Content-type', 'application/json');
+  const params = {
+    username: wname,
+    avatar_url: wavatar,
+    content: msg
+  };
+  request.send(JSON.stringify(params));
+}
+
+sendMessage(product);
