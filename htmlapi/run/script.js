5c60188f09186b3dd0da9b69e20dbb6d81005dfe
jaxodotdev-downloadholder <98191771+jaxodotdev-downloadholder@users.noreply.github.com>
2022-01-29 15:58:25 -0500

    Update script.js

--- htmlapi/run/script.js
+++ htmlapi/run/script.js
@@ -11,6 +11,13 @@ const wname = params.get('name');
 const wavatar = params.get('pfp');
 const wurl = params.get('url');
 
+if (wname=="Bot Name") {
+  wname = "JaxoDev's HTML API Bot";
+}
+if (wavatar=="Bot Picture URL") {
+  wavatar = "";
+}
+
 function sendMessage(msg) {
   const request = new XMLHttpRequest();
   request.open("POST", wurl);
