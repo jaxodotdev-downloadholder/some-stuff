ca9b7422566f06c5fcaef0acfcd8192350ba9c20
jaxodotdev-downloadholder <98191771+jaxodotdev-downloadholder@users.noreply.github.com>
2022-02-04 17:49:08 -0500

    Create import.js

---
+++ webdisc/import.js
@@ -0,0 +1,7 @@
+var head = document.getElementsByTagName('HEAD')[0]; 
+var link = document.createElement('link');
+link.rel = 'stylesheet'; 
+link.type = 'text/css'; 
+function theme(user, file) {
+  link.href = 'https://discord.jaxo.dev/webdisc/themes/'+user+'/'+file+'.css';
+  head.appendChild(link);
